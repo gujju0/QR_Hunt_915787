d4f1328be5d312a8dce8f17c4b46a04712c3733f
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:32:12 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,82 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following are the input devices that enable direct data entry into a computer system from source documents?",
+    answer: "Features",
+    options: [
+      "AV103-B",
+      "AV113-A",
+      "AV010-D",
+      "AV014-C"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following can access the server?",
+    answer: "Quality",
+    options: [
+      "AV009-A",
+      "AV110-A",
+      "AV114-A",
+      "AV001-C"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following service allows a user to log in to another computer somewhere on the Internet?",
+    answer: "Compress",
+    options: [
+      "AV001-B",
+      "AV101-C",
+      "AV008-A",
+      "AV109-C"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following is not a type of computer on the basis of operation?",
+    answer: "Generator",
+    options: [
+      "AV101-A",
+      "AV007-D",
+      "AV108-C",
+      "AV109-C"
+    ]
+  },
+  {
+    numb: 5,
+    question: "The web works on this model.",
+    answer: "Chrome",
+    options: [
+      "AV103-D",
+      "AV012-C",
+      "AV107-B",
+      "AV014-D"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Allows the user to create and maintain a personal list of favorite URL addresses.",
+    answer: "Carry",
+    options: [
+      "AV012-C",
+      "AV109-B",
+      "AV003-A",
+      "AV112-D"
+    ]
+  },
+  {
+    numb: 7,
+    question: "SGML stands for?",
+    answer: "Os",
+    options: [
+      "AV111-A",
+      "AV004-A",
+      "AV010-B",
+      "AV007-A"
+    ]
+  },
+  
+];
